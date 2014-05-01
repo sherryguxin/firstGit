3bcd912428814386c2f074189a3d52ea1bf25b8d
guxin <guxin@192.168.0.103>
2014-05-01 22:55:38 +0800

    1

--- firtTest/src/firtTest/test/TestSrc/TestGit.java
+++ firtTest/src/firtTest/test/TestSrc/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("guxin!!!!!12");
+		System.out.println("guxi11n!1!!!!12");
 		
 	}
 
